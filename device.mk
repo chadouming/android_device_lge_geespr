b8faaad9cf061ae6e59cd597d5fad216941684bb
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2013-06-03 19:42:46 +0000

    Remove liblight, we have it in common

--- device.mk
+++ device.mk
@@ -28,9 +28,6 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/geeb_ca/overlay
 ## common overlays
 DEVICE_PACKAGE_OVERLAYS += device/lge/gee-common/overlay
 
-PRODUCT_PACKAGES := \
-	lights.geeb_ca
-
 # Inherit from gee-common
 $(call inherit-product, device/lge/gee-common/gee-common.mk)
 
