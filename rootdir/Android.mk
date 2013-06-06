8392920309085d97acfa16b7a66cbd80b9789f92
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2013-06-06 12:18:21 +0000

    Fix major derp per @xboxfanj

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -13,6 +13,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.geeb_ca.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.geeb_ca.rc
+LOCAL_SRC_FILES    := etc/ueventd.geeb_ca.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
