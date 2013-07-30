f5a9ac4263f9be0ecf73468d7461b383011a969e
chad cormier roussel <chadcormierroussel@gmail.com>
2013-07-30 19:47:12 -0400

    ADding geebus for us using twrp.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@ TARGET_KERNEL_CONFIG := geeb_defconfig
 -include device/lge/gee-common/BoardConfigCommon.mk
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := geeb_att,geeb,geeb_att_us,e970,geeb_att_us,geeb_ca
+TARGET_OTA_ASSERT_DEVICE := geeb_att,geeb,geeb_att_us,e970,geeb_att_us,geeb_ca,geebus
 
 # board-info
 TARGET_BOARD_INFO_FILE := device/lge/geeb/board-info.txt
