438b1a653d754df817a4d6cb29a4907dc2c88c40
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2013-09-07 15:36:49 +0200

    Add nfc firmware to device tree

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/gee-common/overlay
 # Inherit from gee-common
 $(call inherit-product, device/lge/gee-common/gee-common.mk)
 
+#NFC firmware
+PRODUCT_COPY_FILES := \
+    device/lge/geeb/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
+
 # Telephony Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     telephony.lteOnCdmaDevice=0 \
